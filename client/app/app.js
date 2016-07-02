c9d64076fd20b6f90760d36c50fc0d8214bc9a31
MKS-students <mks.students@hackreactor.com>
2016-07-02 15:28:22 -0700

    herok push 1

--- client/app/app.js
+++ client/app/app.js
@@ -1,24 +1,30 @@
 angular.module('app', [])
   .controller('controller', function($scope, myFactory) {
-    //console.log(myFactory.footBallData)
-    // connect button here
 
     $scope.teams = [];
+    $scope.players = [];
+    $scope.code;
+    $scope.index;
+    $scope.temp;
 
+    $scope.getPlayers = function (data) {
+      //$scope.code = ????;
+      myFactory.getPlayers(data.team._links.players.href).then(function(data){
+      $scope.players = data.data.players;
+      console.log("data", data);
+      console.log("scope is", $scope.players);
+      })
+    }
 
     myFactory.getData().then(function(data) {
-      console.log('=======data', data);
+      //console.log('=======data', data);
       $scope.teams = data;
+      console.log("data",data[1].code);
+      // $scope.code = data
     })
-
-    // myFactory.getPlayers() {
-    //   // $scope.teams = ???
-    // }
-
-
-
   })
   .factory('myFactory', function ($http) {
+
     var getData = function () {
       return $http ({
         method: 'GET',
@@ -29,33 +35,34 @@ angular.module('app', [])
         }
       })
       .then(function(resp) {
-        // console.log('I am inside getData=====', resp)
-        var temp = resp.data.teams.forEach(function(item) {
-          // console.log(item);
-        })
-
+        // var temp = resp.data.teams.forEach(function(item) {
+        //   // console.log(item);
+        // })
+        console.log("resp========",resp.data.teams)
+        // $scope.temp
         return resp.data.teams
       })
     }
 
-
-
-    return {
-      getData: getData
-      //getPlayers: getPlayers
+    var getPlayers = function (linkToPlayers) {
+      return $http ({
+        method: 'GET',
+        url: linkToPlayers,
+        headers: {
+          'X-Auth-Token': "6dbf7da93556476fa7ef33c83a6bbc74"
+        }
+      })
+      .then(function(resp) {
+         // console.log("The response",resp)
+         return resp;
+       })
     }
 
-
-
-
+    return {
+      getData: getData,
+      getPlayers: getPlayers
+    }
 
 
   })
 
-
-//cors header figure out with api
-
-
-
-
-//localhose
--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 var express = require('express');
-var request = require('request');
+//var request = require('request');
 // var bodyParser = require('body-parser');
 var app = express();
 
 
 app.set('port', 8080); //process.env.PORT || 1337
 
-//app.use('/api', routesTasks);
-app.use(express.static('./client')); // everything inside client if public
+
+// everything inside client is public
+app.use(express.static('../client')); // from server.js go to client folder
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ', app.get('port'));
 })
 
 
-
+// process.env.port || 3000 means: whatever is in the environment variable PORT, or 3000 if there's nothing there.
